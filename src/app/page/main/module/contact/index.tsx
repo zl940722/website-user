8f7c3001112e91c83844ba335d609a5bcfa47332
WIN-N9N5LPNC6UL\xyl <wuhuaidong@xinyunlian.com>
2018-08-24 10:55:28 +0800

    contact

--- src/app/page/main/module/contact/index.tsx
+++ src/app/page/main/module/contact/index.tsx
@@ -1,17 +1,47 @@
 import React, {Component} from 'react'
 import {observer} from 'mobx-react'
-
+import * as style from "./style.pcss";
 @observer
 export default class Contact extends Component<any, any> {
 
     public render() {
         return (
             <div>
-                contactwqeqw
+                <CommonBanner/>
+                <Text/>
             </div>
         )
     }
 }
+@observer
+export class CommonBanner extends Component<any, any> {
 
+    public render() {
+        return(
+            <div style={{width: "100%"}}>
+                <img src={require('./images/banner.jpg')} style={{width:"100%"}}/>
+            </div>
+        )
+    }
 
+}
+@observer
+export class Text extends Component<any, any> {
+    public render() {
+        return(
+            <div className={style.contactmid}>
+                <div >
+                    <h2>hr Contact us</h2>
+                    <hr/>
+                </div>
+                <div>
+                    <span>ssss</span>
+                    <span>028-67174822</span>
+                    <span>cantact@</span>
+                    <span>123 Smith Drive,Baltimore,MD 21212</span>
+                </div>
+            </div>
+        )
+    }
+}
 
